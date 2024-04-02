26f9a6b08294df752cd4dbf214bff20b12ae9eef
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-04-02 17:49:02 +0530

    :green_heart: integrated notification flow with pushover

--- poller.ts
+++ poller.ts
@@ -1,3 +1,4 @@
+import pushover from "https://esm.town/v/pranjaldotdev/pushover";
 import { webScrapeBytesNewsletter } from "https://esm.town/v/pranjaldotdev/scraper";
 import { sqlite } from "https://esm.town/v/std/sqlite";
 
@@ -36,8 +37,19 @@ async function checkNewsletterPresent(articleNumber: number) {
     const data = await webScrapeBytesNewsletter();
     const isPresent = await checkNewsletterPresent(data.id);
     if (!isPresent) {
+      console.log(`Article ${data.id} already exists!!!`);
+    } else {
       // insert and notify
       await insertRow(data.id, data.title, data.date);
+
+      const response = await pushover({
+        token: Deno.env.get("PO_API_TOKEN"),
+        user: Deno.env.get("PO_USER_KEY"),
+        title: `Latest bytes.dev newsletter dropped ${data.date}`,
+        message: data.title,
+        url: `https://bytes.dev/archives/${data.id}`,
+      });
+      console.log("Pushover status: ", response.status);
     }
   } catch (err) {
     console.error("Error scraping newsletter ", err);
